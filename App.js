bed6d796b8f6186f2d44d4b1368da5fdfe553b1f
Makar Baderko <makarbaderko@gmail.com>
2020-07-08 19:47:34 +0300

    Created the project

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+
+class App extends Component{
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Hello! My name's Makar</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
+})
